c5fbd8b2e87ecb75036e6096d5f9bf42881714dc
Erik Olofsson <erik@olofsson.info>
2026-06-10 21:30:52 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ set -e
 rm -f *.tar.zst 
 
 PACMAN_PKGBASE=linux-cachyos-imac5k make -j36 pacman-pkg
-sudo pacman -U linux-cachyos-imac5k-7.*_imac5k_*-x86_64.pkg.tar.zst linux-cachyos-imac5k-headers-*_imac5k_*-x86_64.pkg.tar.zst
+if [[ "$1" == "--install" ]]; then
+    sudo pacman -U linux-cachyos-imac5k-7.*_imac5k_*-x86_64.pkg.tar.zst linux-cachyos-imac5k-headers-*_imac5k_*-x86_64.pkg.tar.zst
+fi
